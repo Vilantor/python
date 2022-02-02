a3c1163e58a2ce0b503f655db1f894397f86f24f
Vilantor <46375027+Vilantor@users.noreply.github.com>
2022-02-02 16:34:11 +0300

    Add files via upload

---
+++ HW 3 task_2.py
@@ -0,0 +1,17 @@
+def info(name, surname, birth, sity, email, phonenumb):
+    print(f'Имя: {name}, Фамилия: {surname}, Год рождения: {birth}, Город проживания: {sity}, E-mail: {email}, Телефон: {phonenumb}')
+
+info(name='Владислав', surname='Матюшин', birth='1993', sity='Ростов-на-Дону', email='vladmatyushin@mail.ru', phonenumb='+79188555358')
+
+
+# def info(name, surname, birth, sity, email, phonenumb):
+#     name = str(input('Как Вас зовут?'))
+#     surname = str(input('Ваша фамилия?'))
+#     birth = str(input('Укажите год рождения: '))
+#     sity = str(input('Укажиие город проживания: '))
+#     email = str(input('Укажите e-mail: '))
+#     phonenumb = str(input('Укажите номер телефона: '))
+#     return name, surname, birth, sity, email, phonenumb
+# print(f"Имя: {name} Фамилия: {surname} Год рождения: {birth} Город проживания: {sity} E-mail: {email} Телефон: {phonenumb}")
+#
+# # print(f"Hello, I am {name} and my favorite programming language is {language}")
---
+++ HW 3 task_5.py
@@ -0,0 +1,20 @@
+# a = str(input('Введите через побел числа, сумму которых нужно посчитать: '))
+
+# def addd(*num):
+#     sum = 0
+#     for n in num:
+#         sum += n
+#
+#     print('Сумма: ', sum)
+#
+# print(addd(a=int(input('Введите числа, сумму которых нужно найти: '))))
+
+# addd()
+
+def addd(*num):
+    sum = 0
+    for n in sum:
+        sum += n
+
+
+
